df802271e5eff02356480db32f8ff3b18a60ed04
GoMyungju171 <5211616@likelion.org>
2020-08-06 18:18:25 +0900

    8.6

---
+++ JS/숫자형연습.js
@@ -0,0 +1,12 @@
+var width = 10;
+var height = 20;
+
+// 둘레의 길이 구하기
+var perimeter;
+perimeter = (width+height)*2
+// 넓이 구하기
+var area;
+area = width*height
+
+console.log("둘레의 길이: " + perimeter);
+console.log("넓이: " + area);
---
+++ JS/자료형활용하기2.js
@@ -0,0 +1,17 @@
+// 숫자형과 문자열 재료
+var material1 = '3';
+var material2 = 3;
+var material3 = '4';
+var material4 = 10;
+
+// 요리 결과
+var result1;
+result1 = String(material2*material4)
+var result2;
+result2 = Number(material3)*material4
+
+// 테스트 코드
+console.log(result1);
+console.log(typeof result1);
+console.log(result2);
+console.log(typeof result2);
